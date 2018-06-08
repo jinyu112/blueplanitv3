b34ffa7272b7381fa640228936cf38276ef09444
Alice Guan <aliguan726@gmail.com>
2018-06-07 21:26:38 -0400

    change api back to backend

--- src/components/ApiService.js
+++ src/components/ApiService.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 class ApiService extends Component {
 
   getData(term, latlon, city, date, string_date) {
-      return axios.post('http://matchasensei.com/api', {
+      return axios.post('http://localhost:4200/api', {
           term: term,
           latlon: latlon,
           city: city,
