118ad1016afa321210c1e79d9bf1ef1763d23043
Alice Guan <aliguan726@gmail.com>
2018-07-01 17:48:15 -0400

    implement modal

--- src/components/emailModal.js
+++ src/components/emailModal.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
-import * from 'react-bootstrap';
+import {Modal, Button} from 'react-bootstrap';
 
 class EmailModal extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            modalIsOpen: false
+        };
+
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.onRef(this);
+    }
+
+    componentWillUnmount() {
+        this.props.onRef(undefined);
+    }
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
     render() {
       return (
-        <input className="block btn btn-sm btn-primary moreInfoButton" type="button" value="More Info" onClick={this.handleClick}/>
+          <Modal show={this.state.modalIsOpen} onHide={this.closeModal}>
+           <Modal.Header closeButton>
+             <Modal.Title>Modal heading</Modal.Title>
+           </Modal.Header>
+           <Modal.Body>
+               <p>Hello</p>
+           </Modal.Body>
+           <Modal.Footer>
+             <Button onClick={this.closeModal}>Close</Button>
+           </Modal.Footer>
+         </Modal>
       );
     }
   }
--- src/components/userinput.js
+++ src/components/userinput.js
@@ -22,7 +22,8 @@ import 'react-datepicker/dist/react-datepicker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '../maps.css';
-import emailService from './emailService.js'
+import emailService from './emailService.js';
+import EmailModal from './emailModal.js';
 
 import yelp_logo from '../images/yelp_burst.png';
 import google_logo from '../images/google_places.png';
@@ -93,11 +94,13 @@ class Userinput extends Component {
       message: {},
       allApiData: {}, //this state holds all of the returned api data and is populated from the browser persistent data OR directly from the api calls
       pageNumber: 1,
+      showModal: false,
     };
     this.apiService = new ApiService();
     this.emailService = new emailService();
     this.handleChange = this.handleChange.bind(this);
     this.handleEmail = this.handleEmail.bind(this);
+    this.openModal = this.openModal.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCheckbox = this.handleCheckbox.bind(this);
@@ -508,7 +511,7 @@ handleUserSelectedEventFromDisplayedResults(itinObj_in) {
   }
 
   openModal() {
-
+    this.emailModal.openModal();
   }
 
   handleEmail(e) {
@@ -1154,8 +1157,6 @@ if (!misc.isObjEmpty(this.state.allApiData)) {
               </div>
             </div>
           </div>
-
-
         </div>
         <div className="row eventsCont">
           <div className="col-md-6 itinerary">
@@ -1170,7 +1171,8 @@ if (!misc.isObjEmpty(this.state.allApiData)) {
 
           <div className="mapsfix itinerary col-md-6">
             <div className="sendEmail">
-                <input className="block btn btn-sm btn-primary moreInfoButton" type="button" value="Send Me the Itinerary" onClick={this.openEmailModal}/>
+                <EmailModal onRef={ref => (this.emailModal = ref)}/>
+                <input className="block btn btn-sm btn-primary moreInfoButton" type="button" value="Send Me the Itinerary" onClick={this.openModal}/>
             </div>
             {this.state.resultsArray.length === 0 && this.state.loading === false ? <div className="greeting"><h4>Get Started Planning Your Trip / Day Above!</h4><img alt="globe" src={globe}></img></div> : ' '}
             {this.state.loading === true ? <div className="loader"><Loader type="spinningBubbles" color="#6c757d"></Loader><h5>Searching...</h5></div> :
@@ -1190,6 +1192,7 @@ if (!misc.isObjEmpty(this.state.allApiData)) {
               <GoogleApiWrapper results={this.state.resultsArray} center={this.state.center} />
             </div>
           </div>
+
         </div>
     )
   }
